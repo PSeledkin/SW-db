261510a01c32dfc3338259c5c7fc9f2e64efc0c2
Alex Avtushko <pseledkin@yandex.by>
2021-03-04 01:55:21 +0300

    Initial

---
+++ src/Components/Spinner/spinner.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./spinner.css";
+
+const Spinner = () => {
+  return (
+    <div className="loadingio-spinner-gear-w9f93gnoctk">
+      <div className="ldio-wjy022sl3oj">
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
